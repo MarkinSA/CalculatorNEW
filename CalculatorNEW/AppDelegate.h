e875a78190178b381a94ef1fba38732e9d326223
Admin <admin@mac-admin.local>
2015-11-30 19:32:33 +0400

    Initial Commit

---
+++ CalculatorNEW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorNEW
+//
+//  Created by Admin on 30.11.15.
+//  Copyright © 2015 MarkinSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorNEW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorNEW
+//
+//  Created by Admin on 30.11.15.
+//  Copyright © 2015 MarkinSA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
